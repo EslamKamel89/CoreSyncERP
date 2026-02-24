fe3fe416ab88f00e2b6882e1475b40c31fb8f7a2
EslamKamel89 <elsamkamelforex@gmail.com>
2026-02-24 14:53:06 +0200

    feat(core): register admin role routes and enforce authorization on employee edit

--- Modules/Core/routes/web.php
+++ Modules/Core/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Core\Http\Controllers\CoreController;
 use Modules\Core\Livewire\Settings\CompanyProfile;
+use Modules\Core\Livewire\Roles;
 
 Route::middleware(['auth'])->group(function () {
     Route::get(
@@ -13,4 +14,16 @@ Route::middleware(['auth'])->group(function () {
     Route::prefix('/settings')->name('core.settings.')->group(function () {
         Route::get('/company', CompanyProfile::class)->name('company');
     });
+    Route::prefix('admin')
+        ->name('core.')
+        ->group(function () {
+            Route::get('/roles', Roles\Index::class)
+                ->name('roles.index');
+
+            Route::get('/roles/create', Roles\Create::class)
+                ->name('roles.create');
+
+            Route::get('/roles/{role}/edit', Roles\Edit::class)
+                ->name('roles.edit');
+        });
 });
--- Modules/HR/Livewire/Employees/Edit.php
+++ Modules/HR/Livewire/Employees/Edit.php
@@ -8,7 +8,9 @@ use Modules\HR\Models\Employee;
 
 class Edit extends Component {
     public Employee $employee;
-
+    public function mount(): void {
+        $this->authorize('hr.manage_structure');
+    }
     #[On(Form::SAVED)]
     public function updated(): void {
         $this->redirectRoute('hr.employees.index');
